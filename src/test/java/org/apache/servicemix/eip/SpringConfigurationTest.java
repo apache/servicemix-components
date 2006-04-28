8034673183489eba99b9ce7a50cbb0cada5c3b9d
Guillaume Nodet <gnodet@apache.org>
2006-04-28 08:57:42 +0000

    SM-415: Add a simple NamespaceContext implementation easily configurable from xbean
    git-svn-id: https://svn.apache.org/repos/asf/incubator/servicemix/trunk/servicemix-eip@397808 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/servicemix/eip/SpringConfigurationTest.java
+++ src/test/java/org/apache/servicemix/eip/SpringConfigurationTest.java
@@ -35,7 +35,7 @@ public class SpringConfigurationTest extends SpringTestSupport {
         ServiceMixClient client = new DefaultServiceMixClient(jbi, as);
         InOnly me = client.createInOnlyExchange();
         me.setService(new QName("http://test", "wireTap"));
-        me.getInMessage().setContent(new StringSource("<test><echo/><world/><earth/></test>"));
+        me.getInMessage().setContent(new StringSource("<test xmlns=\"http://test\"><echo/><world/><earth/></test>"));
         client.sendSync(me);
         
         ((Receiver) getBean("trace1")).getMessageList().assertMessagesReceived(1);
