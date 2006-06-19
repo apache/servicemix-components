a904aba4289fcf5c335b19581db413f73fecd24b
Guillaume Nodet <gnodet@apache.org>
2006-06-19 11:12:05 +0000

    Fix service browsing for servicemix-http
    git-svn-id: https://svn.apache.org/repos/asf/incubator/servicemix/trunk/servicemix-http@415292 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/servicemix/http/ServerManager.java
+++ src/main/java/org/apache/servicemix/http/ServerManager.java
@@ -39,7 +39,6 @@ import org.mortbay.jetty.Handler;
 import org.mortbay.jetty.HttpConnection;
 import org.mortbay.jetty.HttpMethods;
 import org.mortbay.jetty.MimeTypes;
-import org.mortbay.jetty.Response;
 import org.mortbay.jetty.Server;
 import org.mortbay.jetty.handler.AbstractHandler;
 import org.mortbay.jetty.handler.ContextHandler;
@@ -307,8 +306,7 @@ public class ServerManager {
     protected class DisplayServiceHandler extends AbstractHandler {
 
         public void handle(String target, HttpServletRequest request, HttpServletResponse response, int dispatch) throws IOException, ServletException {
-            Response base_response = HttpConnection.getCurrentConnection().getResponse();
-            if (response.isCommitted() || base_response.getStatus()!=-1)
+            if (response.isCommitted() || HttpConnection.getCurrentConnection().getRequest().isHandled())
                 return;
             
             String method = request.getMethod();
