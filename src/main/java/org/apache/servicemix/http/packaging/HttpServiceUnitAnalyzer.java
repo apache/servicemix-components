124350fdbf7371fd9a00198cd8288aa1063998da
Philip James Dodds <pdodds@apache.org>
2006-08-08 18:26:03 +0000

    Implemented basic infrastructure for generating the services element of a service unit.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/servicemix/trunk/servicemix-http@429759 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/servicemix/http/packaging/HttpServiceUnitAnalyzer.java
+++ src/main/java/org/apache/servicemix/http/packaging/HttpServiceUnitAnalyzer.java
@@ -1,16 +1,65 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.servicemix.http.packaging;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
+import javax.jbi.messaging.MessageExchange;
+
 import org.apache.servicemix.common.Endpoint;
-import org.apache.servicemix.common.xbean.AbstractXBeanServiceUnitAnalzyer;
+import org.apache.servicemix.common.packaging.Consumes;
+import org.apache.servicemix.common.packaging.ServiceUnitAnalyzer;
+import org.apache.servicemix.common.xbean.AbstractXBeanServiceUnitAnalyzer;
 import org.apache.servicemix.http.HttpEndpoint;
 
-public class HttpServiceUnitAnalyzer extends AbstractXBeanServiceUnitAnalzyer {
+/**
+ * Implementation of the ServiceUnitAnalyzer can be used in tooling to provide a
+ * way to parse the artifact for the service unit and provide a list of consumes
+ * and provides
+ * 
+ * @author Philip Dodds
+ * @since 3.0
+ * @see ServiceUnitAnalyzer, {@link ServiceUnitAnalyzer}
+ * 
+ */
+public class HttpServiceUnitAnalyzer extends AbstractXBeanServiceUnitAnalyzer {
 
 	protected List getConsumes(Endpoint endpoint) {
-		return new ArrayList();
+		List consumesList = new ArrayList();
+		Consumes consumes;
+		if (endpoint.getRole().equals(MessageExchange.Role.CONSUMER)) {
+			consumes = new Consumes();
+			consumes = new Consumes();
+			consumes.setEndpointName(endpoint.getEndpoint());
+			consumes.setInterfaceName(endpoint.getInterfaceName());
+			consumes.setServiceName(endpoint.getService());
+			consumesList.add(consumes);
+		}
+		HttpEndpoint httpEndpoint = (HttpEndpoint) endpoint;
+		consumes = new Consumes();
+		consumes.setEndpointName(httpEndpoint.getTargetEndpoint());
+		consumes.setInterfaceName(httpEndpoint.getTargetInterfaceName());
+		consumes.setServiceName(httpEndpoint.getTargetService());
+		if (consumes.isValid())
+			consumesList.add(consumes);
+		
+		return consumesList;
 	}
 
 	protected String getXBeanFile() {
