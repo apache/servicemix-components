4468141f1491c05716c1cb6e08dccc4aff9d21e7
Gert Vanthienen <gertv@apache.org>
2007-05-28 18:50:05 +0000

    Fix for SM-598: Dynamically determine basic authentication credentials for HTTP
    git-svn-id: https://svn.apache.org/repos/asf/incubator/servicemix/trunk/deployables/bindingcomponents/servicemix-http@542294 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/java/org/apache/servicemix/http/BasicAuthCredentialsTest.java
@@ -0,0 +1,39 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.servicemix.http;
+
+import junit.framework.TestCase;
+
+import org.apache.xbean.spring.context.ClassPathXmlApplicationContext;
+import org.springframework.context.ApplicationContext;
+
+public class BasicAuthCredentialsTest extends TestCase {
+
+    private ApplicationContext context;
+
+    @Override
+    protected void setUp() throws Exception {
+        context = new ClassPathXmlApplicationContext("org/apache/servicemix/http/basic-auth.xml");
+    }
+
+    public void testApplyCredentials() throws Exception {
+        BasicAuthTestSupport support = (BasicAuthTestSupport) context.getBean("BasicAuthTestSupport");
+        assertEquals("testuser", support.getBasicAuth().getUsername().evaluate(null, null));
+        assertEquals("testpass", support.getBasicAuth().getPassword().evaluate(null, null));
+    }
+
+}
