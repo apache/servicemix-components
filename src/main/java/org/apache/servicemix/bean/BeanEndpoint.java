982ef7642fd5f02fb4723d8a6c46dfa312a6c6dc
Guillaume Nodet <gnodet@apache.org>
2007-06-26 12:01:31 +0000

    SM-892: memory leak in org.apache.servicemix.bean.BeanEndpoint
    git-svn-id: https://svn.apache.org/repos/asf/incubator/servicemix/trunk/deployables/serviceengines/servicemix-bean@550780 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/servicemix/bean/BeanEndpoint.java
+++ src/main/java/org/apache/servicemix/bean/BeanEndpoint.java
@@ -255,7 +255,7 @@ public class BeanEndpoint extends ProviderEndpoint implements ApplicationContext
                     }
                 }
             }
-            checkEndOfRequest(req);
+            checkEndOfRequest(req, corId);
             currentRequest.set(null);
         }
     }
@@ -279,7 +279,7 @@ public class BeanEndpoint extends ProviderEndpoint implements ApplicationContext
             me.set(exchange);
             evaluateCallbacks(req);
         }
-        checkEndOfRequest(req);
+        checkEndOfRequest(req, corId);
         currentRequest.set(null);
     }
     
@@ -390,12 +390,12 @@ public class BeanEndpoint extends ProviderEndpoint implements ApplicationContext
         }
     }
     
-    protected void checkEndOfRequest(Request request) {
+    protected void checkEndOfRequest(Request request, Object corId) {
         if (request.getExchange().getStatus() != ExchangeStatus.ACTIVE) {
             ReflectionUtils.callLifecycleMethod(request.getBean(), PreDestroy.class);
             request.setBean(null);
             request.setExchange(null);
-            requests.remove(request);
+            requests.remove(corId);
         }
     }
 
