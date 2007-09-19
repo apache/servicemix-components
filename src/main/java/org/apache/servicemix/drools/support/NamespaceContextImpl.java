962a8c5bad4bb06439fe6d5f00f1a49f1127b355
Gert Vanthienen <gertv@apache.org>
2007-09-19 21:36:53 +0000

    Fix for SM-1056: Add NamespaceContextImpl to servicemix-core
    git-svn-id: https://svn.apache.org/repos/asf/incubator/servicemix/trunk/deployables/serviceengines/servicemix-drools@577452 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/servicemix/drools/support/NamespaceContextImpl.java
+++ src/main/java/org/apache/servicemix/drools/support/NamespaceContextImpl.java
@@ -16,15 +16,6 @@
  */
 package org.apache.servicemix.drools.support;
 
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Set;
-
-import javax.xml.XMLConstants;
-import javax.xml.namespace.NamespaceContext;
 
 /**
  * A simple namespace context with a clean xbean configuration.
@@ -34,133 +25,8 @@ import javax.xml.namespace.NamespaceContext;
  * @author gnodet
  * @version $Revision$
  */
-public class NamespaceContextImpl implements NamespaceContext {
+public class NamespaceContextImpl extends org.apache.servicemix.jbi.jaxp.NamespaceContextImpl {
 
-    /**
-     * map containing bound namespaces, keyed by their prefix. A LinkedHashMap
-     * is used to ensure that {@link #getPrefix(String)} always returns the same
-     * prefix, unless that prefix is removed.
-     */
-    private Map<String, String> namespaces;
-    
-    /**
-     * Constructs a SimpleNamespaceContext with no parent context or namespace
-     * declarations.
-     */
-    public NamespaceContextImpl() {
-        this.namespaces = new LinkedHashMap<String, String>();
-    }
-    
-    /**
-     * Constructs a SimpleNamespaceContext with no parent context that contains
-     * the specified prefixes.
-     * 
-     * @param namespaces A Map of namespace URIs, keyed by their prefixes.
-     */
-    public NamespaceContextImpl(Map<String, String> namespaces) {
-        this.namespaces = new LinkedHashMap<String, String>(namespaces);
-    }
-    
-    /**
-     * @org.apache.xbean.Map entryName="namespace" keyName="prefix"
-     * @return Returns the namespaces.
-     */
-    public Map getNamespaces() {
-        return namespaces;
-    }
-
-    /**
-     * @param namespaces The namespaces to set.
-     */
-    public void setNamespaces(Map<String, String> namespaces) {
-        this.namespaces.clear();
-        if (namespaces != null) {
-            this.namespaces.putAll(namespaces);
-        }
-    }
-
-    /* (non-Javadoc)
-     * @see javax.xml.namespace.NamespaceContext#getNamespaceURI(java.lang.String)
-     */
-    public String getNamespaceURI(String prefix) {
-        if (prefix == null) {
-            throw new IllegalArgumentException("prefix argument was null");
-        } else if (prefix.equals(XMLConstants.XML_NS_PREFIX)) {
-            return XMLConstants.XML_NS_URI;
-        } else if (prefix.equals(XMLConstants.XMLNS_ATTRIBUTE)) {
-            return XMLConstants.XMLNS_ATTRIBUTE_NS_URI;
-        } else if (namespaces.containsKey(prefix)) {
-            String uri = (String) namespaces.get(prefix);
-            if (uri.length() == 0) {
-                return null;
-            } else {
-                return uri;
-            }
-        } else {
-            return null;
-        }
-    }
-
-    /* (non-Javadoc)
-     * @see javax.xml.namespace.NamespaceContext#getPrefix(java.lang.String)
-     */
-    public String getPrefix(String nsURI) {
-        if (nsURI == null) {
-            throw new IllegalArgumentException("nsURI was null");
-        } else if (nsURI.length() == 0) {
-            throw new IllegalArgumentException("nsURI was empty");
-        } else if (nsURI.equals(XMLConstants.XML_NS_URI)) {
-            return XMLConstants.XML_NS_PREFIX;
-        } else if (nsURI.equals(XMLConstants.XMLNS_ATTRIBUTE_NS_URI)) {
-            return XMLConstants.XMLNS_ATTRIBUTE;
-        }
-        Iterator iter = namespaces.entrySet().iterator();
-        while (iter.hasNext()) {
-            Map.Entry entry = (Map.Entry) iter.next();
-            String uri = (String) entry.getValue();
-            if (uri.equals(nsURI)) {
-                return (String) entry.getKey();
-            }
-        }
-        if (nsURI.length() == 0) {
-            return "";
-        } else {
-            return null;
-        }
-    }
-
-    /* (non-Javadoc)
-     * @see javax.xml.namespace.NamespaceContext#getPrefixes(java.lang.String)
-     */
-    public Iterator<String> getPrefixes(String nsURI) {
-        if (nsURI == null) {
-            throw new IllegalArgumentException("nsURI was null");
-        } else if (nsURI.length() == 0) {
-            throw new IllegalArgumentException("nsURI was empty");
-        } else if (nsURI.equals(XMLConstants.XML_NS_URI)) {
-            return Collections.singleton(XMLConstants.XML_NS_PREFIX).iterator();
-        } else if (nsURI.equals(XMLConstants.XMLNS_ATTRIBUTE_NS_URI)) {
-            return Collections.singleton(XMLConstants.XMLNS_ATTRIBUTE).iterator();
-        }
-        Set<String> prefixes = null;
-        for (Map.Entry<String, String> entry : namespaces.entrySet()) {
-            String uri = entry.getValue();
-            if (uri.equals(nsURI)) {
-                if (prefixes == null) {
-                    prefixes = new HashSet<String>();
-                }
-                prefixes.add(entry.getKey());
-            }
-        }
-        if (prefixes != null) {
-            return Collections.unmodifiableSet(prefixes).iterator();
-        } else if (nsURI.length() == 0) {
-            return Collections.singleton("").iterator();
-        } else {
-            prefixes = Collections.emptySet();
-            return prefixes.iterator();
-        }
-    }
     
 }
 
