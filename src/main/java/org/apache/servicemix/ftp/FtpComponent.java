9b14936079d08ed34ca35e0a9f91371297e3aea7
Guillaume Nodet <gnodet@apache.org>
2008-07-03 19:51:35 +0000

    SM-1387: make servicemix-ftp osgi friendly
    git-svn-id: https://svn.apache.org/repos/asf/servicemix/components/bindings/servicemix-ftp/trunk@673780 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/servicemix/ftp/FtpComponent.java
+++ src/main/java/org/apache/servicemix/ftp/FtpComponent.java
@@ -24,8 +24,8 @@ import javax.jbi.servicedesc.ServiceEndpoint;
 
 import org.apache.servicemix.common.DefaultComponent;
 import org.apache.servicemix.common.Endpoint;
-import org.apache.servicemix.jbi.util.IntrospectionSupport;
-import org.apache.servicemix.jbi.util.URISupport;
+import org.apache.servicemix.common.util.IntrospectionSupport;
+import org.apache.servicemix.common.util.URISupport;
 
 /**
  * An FTP based component
