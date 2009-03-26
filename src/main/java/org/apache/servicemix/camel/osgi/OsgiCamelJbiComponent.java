74d7d0a69b318421747e324ff7514d5aa262e85c
Freeman Yue Fang <ffang@apache.org>
2009-03-26 03:31:31 +0000

    [SMXCOMP-483]Camel -> JBI endpoint fails to find converters when deployed into SMX 4
    git-svn-id: https://svn.apache.org/repos/asf/servicemix/components/engines/servicemix-camel/trunk@758508 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/servicemix/camel/osgi/OsgiCamelJbiComponent.java
+++ src/main/java/org/apache/servicemix/camel/osgi/OsgiCamelJbiComponent.java
@@ -19,37 +19,53 @@ package org.apache.servicemix.camel.osgi;
 import java.util.List;
 import java.util.Collections;
 import java.util.ArrayList;
+import java.util.Map;
 import java.io.File;
 import java.net.URL;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.servicemix.camel.CamelJbiComponent;
 import org.apache.servicemix.camel.CamelSpringDeployer;
+import org.apache.servicemix.camel.JbiBinding;
 import org.apache.servicemix.common.BaseServiceUnitManager;
 import org.apache.servicemix.common.Deployer;
 import org.apache.servicemix.common.ServiceMixComponent;
 import org.apache.servicemix.common.xbean.ClassLoaderXmlPreprocessor;
+import org.apache.servicemix.common.xbean.ParentBeanFactoryPostProcessor;
 import org.apache.xbean.spring.context.SpringApplicationContext;
 import org.apache.xbean.classloader.JarFileClassLoader;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.osgi.context.BundleContextAware;
 import org.springframework.osgi.util.BundleDelegatingClassLoader;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.Bundle;
 
 /**
- * When deploying a JBI packaged SU to camel component, camel-spring can not be found
- * by Spring/XBean, thus leading to an exception about the spring namespace not being
- * found.  We need to hack the clasloader for SUs to force a reference to camel-spring
- * in the SU classloader parents.
+ * When deploying a JBI packaged SU to camel component, camel-spring and camel-osgi 
+ * can not be found by Spring/XBean, thus leading to an exception about the spring 
+ * and osgi namespaces not being found.  We need to hack the clasloader for SUs to 
+ * force a reference to camel-spring and camel-osgi in the SU classloader parents.
  *
- * This does not completely solve the problem, as converters can not be found because
- * camel-core ResolverUtil does not work well
+ * We also need to inject the bundleContext into the CamelContextFactoryBean to 
+ * make sure the CamelContextFactoryBean will replace the regular ResolverUtils 
+ * with OSGi ResolverUtils
  */
 public class OsgiCamelJbiComponent extends CamelJbiComponent implements BundleContextAware {
-
+	private static final Log LOG = LogFactory.getLog(OsgiCamelJbiComponent.class);
     private BundleContext bundleContext;
 
     public void setBundleContext(BundleContext bundleContext) {
         this.bundleContext = bundleContext;
+    }
+    
+    public BundleContext getBundleContext() {
+    	return bundleContext;
     }
 
     public BaseServiceUnitManager createServiceUnitManager() {
@@ -58,15 +74,57 @@ public class OsgiCamelJbiComponent extends CamelJbiComponent implements BundleCo
     }
 
     public class OsgiCamelSpringDeployer extends CamelSpringDeployer {
-        public OsgiCamelSpringDeployer(CamelJbiComponent component) {
+        private OsgiCamelJbiComponent osgiCamelJbiComponent;
+    	public OsgiCamelSpringDeployer(OsgiCamelJbiComponent component) {
             super(component);
+            osgiCamelJbiComponent = component;
         }
+        
         protected List getXmlPreProcessors(String serviceUnitRootPath) {
             ClassLoaderXmlPreprocessor classLoaderXmlPreprocessor =
                     new OsgiClassLoaderXmlPreprocessor(new File(serviceUnitRootPath),
                                                        component);
             return Collections.singletonList(classLoaderXmlPreprocessor);
         }
+        
+        protected List getBeanFactoryPostProcessors(String serviceUnitRootPath) {
+            List processors = super.getBeanFactoryPostProcessors(serviceUnitRootPath);
+            // add the post processors to deal with camel context
+            // Parent beans map
+            
+            processors.add(new OsgiBundleContextPostprocessor());
+            
+            return processors;
+        }
+    }
+    
+    private class OsgiBundleContextPostprocessor implements BeanFactoryPostProcessor {    	
+    		
+		public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory)
+				throws BeansException {
+			beanFactory.addBeanPostProcessor(new BeanPostProcessor() {
+				
+				public Object postProcessAfterInitialization(Object bean,
+						String beanName) throws BeansException {
+					// do nothing here
+					return bean;
+				}
+				
+				public Object postProcessBeforeInitialization(Object bean,
+						String beanName) throws BeansException {
+					 if (bean instanceof BundleContextAware) {
+						 BundleContextAware bundleContextAware = (BundleContextAware)bean;
+				         if (bundleContext == null) {
+				             LOG.warn("No bundle defined yet so cannot inject into: " + bean);
+				         } else {
+				             bundleContextAware.setBundleContext(bundleContext);
+				         }
+				     }			         
+				     return bean;
+				}
+			});			
+		}
+    	
     }
 
     public class OsgiClassLoaderXmlPreprocessor extends ClassLoaderXmlPreprocessor {
@@ -79,7 +137,8 @@ public class OsgiCamelJbiComponent extends CamelJbiComponent implements BundleCo
             parents.add(super.getParentClassLoader(applicationContext));
             for (Bundle bundle : bundleContext.getBundles()) {
                 try {
-                    if (bundle.getSymbolicName().contains("camel-spring")) {
+                	String symbolicName = bundle.getSymbolicName();
+                    if (symbolicName.contains("camel-spring") || symbolicName.contains("camel-osgi")) {
                         parents.add(BundleDelegatingClassLoader.createBundleClassLoaderFor(bundle));
                     }
                 } catch (Throwable e) {
