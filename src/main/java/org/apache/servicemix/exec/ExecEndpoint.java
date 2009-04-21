664b4fea7a57ef8afd03c00b0df2661c50bf8c42
Jean-Baptiste Onofre <jbonofre@apache.org>
2009-04-21 13:45:55 +0000

    Add marshaler to parse and extract command from the in normalized message.
    git-svn-id: https://svn.apache.org/repos/asf/servicemix/components/engines/servicemix-exec/trunk@767147 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/servicemix/exec/ExecEndpoint.java
+++ src/main/java/org/apache/servicemix/exec/ExecEndpoint.java
@@ -19,10 +19,15 @@ package org.apache.servicemix.exec;
 import javax.jbi.messaging.ExchangeStatus;
 import javax.jbi.messaging.InOnly;
 import javax.jbi.messaging.MessageExchange;
+import javax.jbi.messaging.NormalizedMessage;
 import javax.jbi.messaging.RobustInOnly;
 import javax.jbi.messaging.MessageExchange.Role;
 
-import org.apache.servicemix.common.endpoints.ConsumerEndpoint;
+import org.apache.servicemix.common.endpoints.ProviderEndpoint;
+import org.apache.servicemix.exec.utils.ExecMarshaler;
+import org.apache.servicemix.exec.utils.ExecUtils;
+import org.apache.servicemix.jbi.jaxp.SourceTransformer;
+import org.apache.servicemix.jbi.jaxp.StringSource;
 
 /**
  * Represents an exec endpoint.
@@ -30,13 +35,15 @@ import org.apache.servicemix.common.endpoints.ConsumerEndpoint;
  * @author jbonofre
  * @org.apache.xbean.XBean element="endpoint"
  */
-public class ExecEndpoint extends ConsumerEndpoint {
+public class ExecEndpoint extends ProviderEndpoint {
     
     private String command; // the command can be static (define in the descriptor) or provided in the incoming message
     
+    private SourceTransformer transformer = new SourceTransformer();
+    
     @Override
     public void process(MessageExchange exchange) throws Exception {
-        // for now, the component acts as a provider, this means that another component has requested our service
+        // The component acts as a provider, this means that another component has requested our service
         // As this exchange is active, this is either an in or a fault (out are sent by this component)
         if (exchange.getRole() == Role.PROVIDER) {
             if (exchange.getStatus() == ExchangeStatus.DONE) {
@@ -49,19 +56,19 @@ public class ExecEndpoint extends ConsumerEndpoint {
                 // exchange is active
                 this.handleProviderExchange(exchange);
             }
-        } else {
-            // exchange role is not yet supported (consumer)
+        }
+        // unsupported role
+        else {
             throw new IllegalStateException("Unsupported role: " + exchange.getRole());
         }
     }
     
     /**
      * <p>
-     * Handlers on the message exchange (provider role).
+     * Handles on the message exchange (provider role).
      * </p>
      * 
      * @param exchange the <code>MessageExchange</code>.
-     * @throws Exception if the <code>MessageExchange</code> doesn't contain in message or if an unexpected error occurs.
      */
     protected void handleProviderExchange(MessageExchange exchange) throws Exception {
         // fault message
@@ -74,11 +81,27 @@ public class ExecEndpoint extends ConsumerEndpoint {
                 if (logger.isDebugEnabled()) {
                     logger.debug("Received exchange: " + exchange);
                 }
-                // TODO parse the in message, extract command/args and execute command
+                // gets the in message
+                NormalizedMessage in = exchange.getMessage("in");
+                // parses the in message and get the execution command
+                String exec = ExecMarshaler.constructExecCommand(in);
+                // executes the command
+                String output = ExecUtils.execute(exec);
+                // push the exec output in out message
+                NormalizedMessage out = exchange.createMessage();
+                out.setContent(new StringSource("<output>" + output + "</output>"));
+                exchange.setMessage(out, "out");
             }
         } else {
-            // the message exchange is not compliant with the default MEP
-            throw new IllegalStateException("Provider exchange is ACTIVE, but no in or fault is provided.");
+            if (command != null) {
+                // executes the user-defined command
+                String output = ExecUtils.execute(command);
+                NormalizedMessage out = exchange.createMessage();
+                out.setContent(new StringSource("<output>" + output + "</output>"));
+                exchange.setMessage(out, "out");
+            } else {
+                throw new IllegalStateException("Provider exchange is ACTIVE, but no fault or command is provided.");
+            }
         }
     }
 
---
+++ src/main/java/org/apache/servicemix/exec/utils/ExecMarshaler.java
@@ -0,0 +1,72 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.servicemix.exec.utils;
+
+import javax.jbi.messaging.NormalizedMessage;
+import javax.xml.transform.TransformerException;
+
+import org.apache.servicemix.jbi.jaxp.SourceTransformer;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+
+/**
+ * This utility class parse the content of a message coming from the NMR
+ * and constructs the execution command.
+ * 
+ * @author jbonofre
+ */
+public class ExecMarshaler {
+
+    private static final String TAG_COMMAND = "command";
+    private static final String TAG_ARGUMENT = "argument";
+    
+    /**
+     * <p>
+     * Parses the in normalized message, extracts command and arguments from the content
+     * and constructs the execution command.
+     * </p>
+     * 
+     * @param message the in <code>NormalizedMessage</code>.
+     * @return the execution command.
+     */
+    public static final String constructExecCommand(NormalizedMessage message) throws TransformerException {
+        String execString = null;
+        // create a source transformer
+        SourceTransformer transformer = new SourceTransformer();
+        try {
+            Document document = transformer.toDOMDocument(message);
+            document.getDocumentElement().normalize();
+            
+            NodeList commandNode = document.getElementsByTagName(TAG_COMMAND);
+            if (commandNode == null || commandNode.getLength() != 1) {
+                throw new TransformerException("Invalid message content. Missing tag: " + TAG_COMMAND);
+            }
+            execString = commandNode.item(0).getChildNodes().item(0).getNodeValue();
+            
+            NodeList argumentNodes = document.getElementsByTagName(TAG_ARGUMENT);
+            for (int i = 0; i < argumentNodes.getLength(); i++) {
+                execString = execString + argumentNodes.item(i).getChildNodes().item(0).getNodeValue();
+            }
+            
+        } catch (Exception e) {
+            throw new TransformerException(e);
+        }
+        return execString;
+    }
+    
+    
+}
