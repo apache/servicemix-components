546b7e1271b9224dfa5163b9a1faecf4cc94744a
Jean-Baptiste Onofre <jbonofre@apache.org>
2009-04-21 07:50:12 +0000

    Add the component bean descriptor. Begin the endpoint provider process.
    git-svn-id: https://svn.apache.org/repos/asf/servicemix/components/engines/servicemix-exec/trunk@767061 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/servicemix/exec/ExecEndpoint.java
+++ src/main/java/org/apache/servicemix/exec/ExecEndpoint.java
@@ -16,24 +16,70 @@
  */
 package org.apache.servicemix.exec;
 
+import javax.jbi.messaging.ExchangeStatus;
+import javax.jbi.messaging.InOnly;
 import javax.jbi.messaging.MessageExchange;
+import javax.jbi.messaging.RobustInOnly;
+import javax.jbi.messaging.MessageExchange.Role;
 
 import org.apache.servicemix.common.endpoints.ConsumerEndpoint;
 
 /**
- * ServiceMix Exec component endpoint
+ * Represents an exec endpoint.
  * 
  * @author jbonofre
  * @org.apache.xbean.XBean element="endpoint"
  */
 public class ExecEndpoint extends ConsumerEndpoint {
     
-    private String command;
-    private int timeout;
+    private String command; // the command can be static (define in the descriptor) or provided in the incoming message
     
-    // TODO
-    public void process(MessageExchange exchange) {
-        // TODO
+    @Override
+    public void process(MessageExchange exchange) throws Exception {
+        // for now, the component acts as a provider, this means that another component has requested our service
+        // As this exchange is active, this is either an in or a fault (out are sent by this component)
+        if (exchange.getRole() == Role.PROVIDER) {
+            if (exchange.getStatus() == ExchangeStatus.DONE) {
+                // exchange is finished
+                return;
+            } else if (exchange.getStatus() == ExchangeStatus.ERROR) {
+                // exchange has been aborted with an exception
+                return;
+            } else {
+                // exchange is active
+                this.handleProviderExchange(exchange);
+            }
+        } else {
+            // exchange role is not yet supported (consumer)
+            throw new IllegalStateException("Unsupported role: " + exchange.getRole());
+        }
+    }
+    
+    /**
+     * <p>
+     * Handlers on the message exchange (provider role).
+     * </p>
+     * 
+     * @param exchange the <code>MessageExchange</code>.
+     * @throws Exception if the <code>MessageExchange</code> doesn't contain in message or if an unexpected error occurs.
+     */
+    protected void handleProviderExchange(MessageExchange exchange) throws Exception {
+        // fault message
+        if (exchange.getFault() != null) {
+            done(exchange);
+        } else if (exchange.getMessage("in") != null) {
+            // in message presents
+            if (exchange instanceof InOnly || exchange instanceof RobustInOnly) {
+                // the MEP is InOnly based
+                if (logger.isDebugEnabled()) {
+                    logger.debug("Received exchange: " + exchange);
+                }
+                // TODO parse the in message, extract command/args and execute command
+            }
+        } else {
+            // the message exchange is not compliant with the default MEP
+            throw new IllegalStateException("Provider exchange is ACTIVE, but no in or fault is provided.");
+        }
     }
 
 }
