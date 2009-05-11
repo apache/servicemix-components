4cfe61a79c01f5be873e5fae64a3a0321b553afd
Gert Vanthienen <gertv@apache.org>
2009-05-11 15:16:52 +0000

    SMXCOMP-534: Two new tests for the servicemix-file sender endpoint
    git-svn-id: https://svn.apache.org/repos/asf/servicemix/components/bindings/servicemix-file/trunk@773597 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/servicemix/file/PollDirectoryTest.java
+++ src/test/java/org/apache/servicemix/file/PollDirectoryTest.java
@@ -16,7 +16,12 @@
  */
 package org.apache.servicemix.file;
 
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import javax.jbi.messaging.ExchangeStatus;
 import javax.jbi.messaging.InOnly;
@@ -97,6 +102,64 @@ public class PollDirectoryTest extends SpringTestSupport {
         numFiles = dir.listFiles().length;
         assertEquals("There shouldn't be any files in the poll folder...but I found " + numFiles + " files there..." , 0, numFiles);
     }
+
+    // Testing the "append=false" 
+    public void testSendToWriterNotAppend() throws Exception {
+        FileUtil.deleteFile(new File("target/pollerFilesNotAppend"));
+     
+        // now lets make a request on this endpoint
+        DefaultServiceMixClient client = new DefaultServiceMixClient(jbi);
+
+        // lets send a request to be written to a file
+        InOnly me = client.createInOnlyExchange();
+        me.setService(new QName("urn:test", "serviceNotAppend"));
+        NormalizedMessage message = me.getInMessage();
+        message.setProperty(DefaultFileMarshaler.FILE_NAME_PROPERTY, "test.xml");
+        message.setContent(new StringSource("<hello>world</hello>"));
+        client.sendSync(me);
+
+        // send a second request so that it should overwrite the first 
+        // in the test.xml file
+        InOnly me2 = client.createInOnlyExchange();
+        me2.setService(new QName("urn:test", "serviceNotAppend"));
+        NormalizedMessage message2 = me2.getInMessage();
+        message2.setProperty(DefaultFileMarshaler.FILE_NAME_PROPERTY, "test.xml");
+        message2.setContent(new StringSource("<Goodbye>world</Goodbye>"));
+        client.sendSync(me2);            
+
+        File resultFile = new File("target/pollerFilesNotAppend/test.xml");
+        String content = getFileContent(resultFile);
+        assertEquals(content.indexOf("hello"), -1);
+    }
+        
+    // Testing the "append=true" 
+    public void testSendToWriterAppend() throws Exception {
+        FileUtil.deleteFile(new File("target/pollerFilesAppend"));
+     
+        // now lets make a request on this endpoint
+        DefaultServiceMixClient client = new DefaultServiceMixClient(jbi);
+
+        // lets send a request to be written to a file        
+        InOnly me = client.createInOnlyExchange();
+        me.setService(new QName("urn:test", "serviceAppend"));
+        NormalizedMessage message = me.getInMessage();
+        message.setProperty(DefaultFileMarshaler.FILE_NAME_PROPERTY, "test.xml");
+        message.setContent(new StringSource("<hello>world</hello>"));
+        client.sendSync(me);
+
+        // send a second request so that it append to the first 
+        // request in the test.xml file
+        InOnly me2 = client.createInOnlyExchange();
+        me2.setService(new QName("urn:test", "serviceAppend"));
+        NormalizedMessage message2 = me2.getInMessage();
+        message2.setProperty(DefaultFileMarshaler.FILE_NAME_PROPERTY, "test.xml");
+        message2.setContent(new StringSource("<Goodbye>world</Goodbye>"));
+        client.sendSync(me2);            
+
+        File resultFile = new File("target/pollerFilesAppend/test.xml");       
+        String content = getFileContent(resultFile);
+        assertTrue(content.indexOf("hello") != 0);
+    }    
    
     protected void assertExchangeWorked(MessageExchange me) throws Exception {
         if (me.getStatus() == ExchangeStatus.ERROR) {
@@ -113,5 +176,35 @@ public class PollDirectoryTest extends SpringTestSupport {
     protected AbstractXmlApplicationContext createBeanFactory() {
         return new ClassPathXmlApplicationContext("spring-polling.xml");
     }
+
+    private String getFileContent(File file) {
+        FileInputStream fis = null;
+        BufferedInputStream bis = null;
+        DataInputStream dis = null;
+        String str = "";
+
+        try {
+            fis = new FileInputStream(file);
+            // Here BufferedInputStream is added for fast reading.
+            bis = new BufferedInputStream(fis);
+            dis = new DataInputStream(bis);
+
+            // dis.available() returns 0 if the file does not have more lines.
+            while (dis.available() != 0) {
+                // this statement reads the line from the file and print it to
+                // the console.
+                str = str.concat(dis.readLine());
+            }  
+            // dispose all the resources after using them.
+            fis.close();
+            bis.close();
+            dis.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return str;
+    }            
 
 }
