cb3bfa4830344d96c1a3918d55758d73bb980db9
Jean-Baptiste Onofre <jbonofre@apache.org>
2009-10-05 12:31:50 +0000

    Begin to implement component unit test using pax-exam to be able to test the component on SMX4/NMR/JBI.
    git-svn-id: https://svn.apache.org/repos/asf/servicemix/components/engines/servicemix-exec/trunk@821774 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/java/org/apache/servicemix/exec/itest/smx4/AbstractFeatureTest.java
@@ -0,0 +1,64 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.servicemix.exec.itest.smx4;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.junit.After;
+import org.junit.Before;
+import org.ops4j.pax.exam.Inject;
+import org.ops4j.pax.exam.Option;
+import org.osgi.framework.BundleContext;
+
+import static org.ops4j.pax.exam.CoreOptions.felix;
+import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
+import static org.ops4j.pax.exam.CoreOptions.options;
+import static org.ops4j.pax.exam.container.def.PaxRunnerOptions.profile;
+import static org.ops4j.pax.exam.container.def.PaxRunnerOptions.scanFeatures;
+
+/**
+ * <p>
+ * Abstract class to manage feature/bundle unit testing.
+ * </p>
+ * 
+ * @author jbonofre
+ */
+public class AbstractFeatureTest {
+    
+    protected final transient Log LOG = LogFactory.getLog(getClass());
+    
+    @Inject
+    protected BundleContext bundleContext;
+    
+    @Before
+    public void setUp() throws Exception {
+    }
+    
+    @After
+    public void tearDown() throws Exception {
+    }
+    
+    public static Option[] configure(String feature) {
+        Option[] options = options(
+                profile("log").version("1.4"),
+                org.ops4j.pax.exam.CoreOptions.systemProperty("org.ops4j.pax.logging.DefaultServiceLog.level").value("DEBUG"),
+                scanFeatures(mavenBundle().groupId("org.apache.servicemix.nmr").artifactId("apache-servicemix-nmr").version("1.1.0-SNAPSHOT").type("xml/features"), "jbi"), felix());
+        return options;
+    }
+
+}
